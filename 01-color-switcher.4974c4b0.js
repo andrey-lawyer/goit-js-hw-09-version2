241b94a00d80662c8603ecb9d43a1ca3e918625d
andrey-lawyer <113498656+andrey-lawyer@users.noreply.github.com>
2022-11-07 15:20:42 +0000

    Deploying to gh-pages from @ andrey-lawyer/goit-js-hw-09-version2@b832016b5b7f08489acd12a3a7cf256338188a4b 🚀

---
+++ 01-color-switcher.4974c4b0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");t.disabled=!0;var a=null;e.addEventListener("click",(function(){t.disabled=!1,(a=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3))&&(e.disabled=!0)})),t.addEventListener("click",(function(){clearInterval(a),t.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.4974c4b0.js.map
---
+++ 01-color-switcher.73b34dfb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");t.disabled=!0;let a=null;e.addEventListener("click",(function(){t.disabled=!1,a=setInterval((()=>d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),a&&(e.disabled=!0)})),t.addEventListener("click",(function(){clearInterval(a),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.73b34dfb.js.map
